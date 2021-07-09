40e3e81a3885c9b1ccd290674f3c607d59d2ac67
champix56 <desorbaix@free.fr>
2021-07-09 16:05:20 +0100

    fin with test

---
+++ src/App/App.test.tsx
@@ -0,0 +1,8 @@
+import {screen,render} from '@testing-library/react';
+import App from './App';
+describe('App', () => {
+    test('it should return value a index position in state', () => {
+        const app=new App(undefined);
+        expect(app.getValue(0)).toBe(15)
+    });
+});
